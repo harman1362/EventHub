edecd0e7a8bc9b392774465243ac77bc1662ea86
Harmanpreet Singh <pb65.harman@gmail.com>
2023-11-03 16:47:06 -0400

    event register

--- frontend/src/pages/EventRegister.js
+++ frontend/src/pages/EventRegister.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import './EventRegister.css'
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
 
 function EventRegister() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
     eventName: '',
-    eventDate: '',
+    location: '',
+    eventDescription: '',
+    category: '',
+    date: ''
   });
 
   const handleChange = (e) => {
@@ -19,13 +24,21 @@ function EventRegister() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log();
     console.log(formData);
-    setFormData({
-      name: '',
-      email: '',
-      eventName: '',
-      eventDate: '',
-    });
+    const endpoint = 'http://localhost:2300/events';
+
+    axios
+      .post(endpoint, formData)
+      .then((response) => {
+        alert("Event Registered Successful")
+        console.log('Request was successful:', response.data);
+        navigate('/events')
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
   };
 
   return (
@@ -33,26 +46,6 @@ function EventRegister() {
       <h2>Event Registration</h2>
       <div className="event-registration-container">
         <form onSubmit={handleSubmit}>
-          <div>
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
           <div>
             <label>Event Name:</label>
             <input
@@ -64,11 +57,41 @@ function EventRegister() {
             />
           </div>
           <div>
-            <label>Event Date:</label>
+            <label>Location:</label>
+            <input
+              type="text"
+              name="location"
+              value={formData.location}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Event Description:</label>
+            <input
+              type="text"
+              name="eventDescription"
+              value={formData.eventDescription}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Category:</label>
+            <input
+              type="text"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Date:</label>
             <input
               type="date"
-              name="eventDate"
-              value={formData.eventDate}
+              name="date"
+              value={formData.date}
               onChange={handleChange}
               required
             />
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,40 @@
+import axios from "axios";
+import React, { useState } from "react";
+
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        const endpoint = 'http://localhost:2300/signup';
+        console.log(formData);
+        axios
+            .post(endpoint, formData)
+            .then((response) => {
+                alert("New User Registered")
+                console.log('Request was successful:', response.data);
+            })
+
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+    };
+
     return (
         <>
             <section class="bg-gray-50 dark:bg-gray-900">
@@ -13,14 +48,18 @@ const Register = () => {
                             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Create and account
                             </h1>
-                            <form class="space-y-4 md:space-y-6" action="#">
+                            <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                                 <div>
                                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""
+                                        value={formData.email}
+                                        onChange={handleChange} />
                                 </div>
                                 <div>
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
+                                        value={formData.password}
+                                        onChange={handleChange} />
                                 </div>
                                 <div>
                                     <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
