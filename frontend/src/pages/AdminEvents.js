53f1c93e03f7ff63b94f56f1ea85780d7df465c8
Harmanpreet Singh <pb65.harman@gmail.com>
2023-11-03 23:03:47 -0400

    ui changes

--- frontend/src/pages/AdminEvents.js
+++ frontend/src/pages/AdminEvents.js
@@ -21,82 +21,117 @@ function AdminEvents() {
 
     const eventApproved = (eventId) => {
 
-    const apiUrl = `http://localhost:2300/events/eventStatus/${eventId}`;     
+        const apiUrl = `http://localhost:2300/events/eventStatus/${eventId}`;
 
-    // Create an object with the data you want to send in the request body
-    const requestData = {
-      approvalStatus: 'approved',
-    };
+        // Create an object with the data you want to send in the request body
+        const requestData = {
+            approvalStatus: 'approved',
+        };
 
-    // Send the PUT request using Axios
-    axios.put(apiUrl, requestData)
-      .then(response => {
-        console.log('PUT request successful', response.data);
-        // Handle the response data as needed
-        alert("Event status updated!!!")
-      })
-      .catch(error => {
-        console.error('Error sending PUT request', error);
-        // Handle any errors
-      });
+        // Send the PUT request using Axios
+        axios.put(apiUrl, requestData)
+            .then(response => {
+                console.log('PUT request successful', response.data);
+                // Handle the response data as needed
+                alert("Event status updated!!!")
+            })
+            .catch(error => {
+                console.error('Error sending PUT request', error);
+                // Handle any errors
+            });
     }
 
     const eventRejected = (eventId) => {
 
-        const apiUrl = `http://localhost:2300/events/eventStatus/${eventId}`;     
-    
+        const apiUrl = `http://localhost:2300/events/eventStatus/${eventId}`;
+
         // Create an object with the data you want to send in the request body
         const requestData = {
-          approvalStatus: 'rejected',
+            approvalStatus: 'rejected',
         };
-    
+
         // Send the PUT request using Axios
         axios.put(apiUrl, requestData)
-          .then(response => {
-            console.log('PUT request successful', response.data);
-            // Handle the response data as needed
-            alert("Event status updated!!!")
-          })
-          .catch(error => {
-            console.error('Error sending PUT request', error);
-            // Handle any errors
-          });
-        }
+            .then(response => {
+                console.log('PUT request successful', response.data);
+                // Handle the response data as needed
+                alert("Event status updated!!!")
+            })
+            .catch(error => {
+                console.error('Error sending PUT request', error);
+                // Handle any errors
+            });
+    }
 
 
     return (
-        
+
         <div>
-            <h2>Registered Events</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Event Name</th>
-                        <th>Location</th>
-                        <th>Event Description</th>
-                        <th>Category</th>
-                        <th>Date</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {events.map((event, index) => (
-                        <tr key={index}>
-                            <td>{event.eventName}</td>
-                            <td>{event.location}</td>
-                            <td>{event.eventDescription}</td>
-                            <td>{event.category}</td>
-                            <td>{event.date} </td>
-                            <td>{event.approvalStatus}</td>
-                            <td>
-                                <button onClick={() => eventApproved(event._id)}>Approve</button>
-                                <button onClick={() => eventRejected(event._id)}>Reject</button>
-                            </td>
+
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                        Registered Events
+                        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of All the Events at one place and the admin has the Authority to Approve or Reject any Event .</p>
+                    </caption>
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Event Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Location
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Event Description
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Date
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Status
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
                         </tr>
+                    </thead>
+
+                    {events.map((event, index) => (
+                        <tbody>
+                            <tr key={index} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-950 whitespace-nowrap dark:text-white">
+                                    {event.eventName}
+                                </th>
+                                <td class="px-6 py-4" className="text-black">
+                                    {event.location}
+                                </td>
+                                <td class="px-6 py-4" className="text-black">
+                                    {event.eventDescription}
+                                </td>
+                                <td class="px-6 py-4" className="text-black">
+                                    {event.category}
+                                </td>
+                                <td class="px-6 py-4" className="text-black">
+                                    {event.date}
+                                </td>
+                                <td class="px-6 py-4" className="text-black">
+                                    {event.approvalStatus}
+                                </td>
+                                <td class="px-6 py-4 text-right">
+                                    <button class="font-medium text-blue-600 dark:text-blue-500 hover:underline" onClick={() => eventApproved(event._id)}>Approve</button>
+                                    <button class="font-medium text-red-600 dark:text-red-500 hover:underline" onClick={() => eventRejected(event._id)}>Reject</button>
+                                </td>
+                            </tr>
+                        </tbody>
                     ))}
-                </tbody>
-            </table>
+
+                </table>
+            </div>
+
         </div>
     );
 }
--- frontend/src/pages/EventCard.js
+++ frontend/src/pages/EventCard.js
@@ -5,14 +5,14 @@ const EventCard = (props) => {
     return (
         <>
 
-            <div class="p-4 lg:w-1/3">
-                <div class="h-full bg-gray-100 bg-opacity-75 px-8  pb-24 rounded-lg overflow-hidden text-center relative">
+            <div class="p-2">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8  pb-6 rounded-lg overflow-hidden text-center relative">
                     <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{props.event.category}</h2>
-                    <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{props.event.eventName}</h1>  
+                    <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{props.event.eventName}</h1>
                     <p class="leading-relaxed mb-3">{props.event.eventDescription}</p>
                     <p class="leading-relaxed mb-3">{props.event.location}</p>
                     <p class="leading-relaxed mb-3">{props.event.date}</p>
-                    <button className="custom-button">Register Event</button>
+                    <button className="custom-button">Register </button>
 
                 </div>
             </div>
